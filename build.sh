780b76632780dbe727e09f7be43e5c1bb3131f67
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2018-03-11 13:36:35 +0100

    Building in docker gradle image and running by docker-compose

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+DOCKER_IMAGE="gradle:jdk8"
+GRADLE_HOME_VOLUME="gradlehome"
+GRADLE_CMD="clean build"
+
+docker run -t --rm --name solideo-www-spring \
+ -v "GRADLE_HOME_VOLUME":/home/gradle/.gradle -v "$DIR":/home/gradle/project \
+ -w /home/gradle/project $DOCKER_IMAGE gradle $GRADLE_CMD
