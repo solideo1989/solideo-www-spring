3574d3b79dc92e1e5c903906c21a5b1f3ad3bf3f
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2018-03-12 19:56:31 +0100

    Automated docker deployment scripts

--- build.sh
+++ build.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
-#https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+##############
+## Settings ##
+##############
 DOCKER_IMAGE="gradle:jdk8"
 GRADLE_HOME_VOLUME="gradlehome"
-GRADLE_CMD="clean build"
+GRADLE_CMD="clean test build $SD_GRADLE_OPTIONS"
+
+
+
+
+
+#####################
+## Build on Docker ##
+#####################
+echo "Gradle options: [$SD_GRADLE_OPTIONS]";
+
+#https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 docker run -t --rm --name solideo-www-spring-builder \
  -v "$GRADLE_HOME_VOLUME":/home/gradle/.gradle -v "$DIR":/home/gradle/project \
@@ -13,7 +26,10 @@ docker run -t --rm --name solideo-www-spring-builder \
  -w /home/gradle/project $DOCKER_IMAGE gradle $GRADLE_CMD \
  2>&1 | tee build.log
 
-echo "Build finished"
+echo ""
+echo ""
 
-#    Bug in gradle: Running as "gradle" user causes: "Failed to load native library 
-# 'libnative-platform.so' for Linux amd64". Fix: "-u root"
+
+
+# Bug in gradle: Running as gradle user causes: Failed to load native library 
+# 'libnative-platform.so' for Linux amd64. Fix: "-u root"
